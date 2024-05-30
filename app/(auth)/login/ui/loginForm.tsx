54528293a653a619a1941273777bd515ac381d5b
joel-danjuma <leojjad@gmail.com>
2024-05-30 13:44:59 +0100

    update server actions and add pasword visibility feature

--- app/(auth)/login/ui/loginForm.tsx
+++ app/(auth)/login/ui/loginForm.tsx
@@ -1,122 +1,127 @@
-"use client"
-import * as z from "zod"
-import { login } from "@/actions/actions"
-import { LoginSchema } from "@/schemas"
-import { useForm } from "react-hook-form"
-import FormError from "../../ui/form-error"
-import { Input } from "@/components/ui/gradient-input"
-import FormSuccess from "../../ui/form-success"
-import { Button } from "@/components/ui/button"
-import { startTransition, useState } from "react"
-import { zodResolver } from "@hookform/resolvers/zod"
+"use client";
 import {
-    Form,
-    FormField,
-    FormControl,
-    FormItem,
-    FormMessage,
-} from "@/components/ui/form"
+  Form,
+  FormField,
+  FormControl,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
+import { login } from "@/actions/actions";
+import { useForm } from "react-hook-form";
+import { Input } from "@nextui-org/input";
+import FormError from "../../ui/form-error";
+import FormSuccess from "../../ui/form-success";
+import { Button } from "@/components/ui/button";
+import { startTransition, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { EyeFilledIcon } from "../../ui/eyeFilledIcon";
+// import { Input } from "@/components/ui/gradient-input";
+import { EyeSlashFilledIcon } from "../../ui/eyeSlashedFilled";
 
 const LoginForm = () => {
-    const [error, setError] = useState<string | undefined>("")
-    const [success, setSuccess] = useState<string | undefined>("")
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleVisibility = () => setIsVisible(!isVisible);
 
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
-        defaultValues: {
-            email: "",
-            password: "",
-        },
-    })
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    const onSubmit = async (values: z.infer<typeof LoginSchema>) => {
-        // client-side-validation
-        setError("")
-        setSuccess("")
-        startTransition(() => {
-            login(values).then((data) => {
-                if (data) {
-                    setError(data.error)
-                    setSuccess(data.success)
-                } else {
-                    return
-                }
-            })
-        })
-    }
+  const onSubmit = async (values: z.infer<typeof LoginSchema>) => {
+    // client-side-validation
+    setError("");
+    setSuccess("");
+    startTransition(() => {
+      login(values).then((data) => {
+        if (data) {
+          setError(data.error);
+          setSuccess(data.success);
+        } else {
+          return;
+        }
+      });
+    });
+  };
 
-    return (
-        <Form {...form}>
-            <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="grid gap-4 min-w-[320px] min-h-[200px]"
-            >
-                <div className="grid gap-2">
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem className="w-full">
-                                <FormControl className="w-full">
-                                    <Input
-                                        {...field}
-                                        name="email"
-                                        type="email"
-                                        placeholder="m@example.com"
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                </div>
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="grid gap-4 min-w-[320px] min-h-[200px]"
+      >
+        <div className="grid gap-2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl className="w-full">
+                  <Input
+                    {...field}
+                    name="email"
+                    type="email"
+                    placeholder="m@example.com"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
 
-                <div className="grid gap-2">
-                    <FormField
-                        control={form.control}
-                        name="password"
-                        render={({ field }) => (
-                            <FormItem className="w-full ">
-                                <FormControl className="w-full">
-                                    <Input
-                                        {...field}
-                                        name="password"
-                                        type={"password"}
-                                        placeholder={"8+ Characters"}
-                                    />
-                                    {/* <div className="w-full flex items-end">
-                   
-                    <Button
-                      className="rounded-none border-l-0 border-r-0 border-t-0 border-b-2 "
-                      variant="outline"
-                      type="button"
-                      onClick={toggleVisibility}
-                    >
-                      {isVisible ? <EyeSlashFilledIcon /> : <EyeFilledIcon />}
-                    </Button>
-                  </div> */}
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                </div>
-                <div className="grid gap-2">
-                    <div className="flex items-center">
-                        {/* <Link href="#" className="ml-auto inline-block text-sm underline">
+        <div className="grid gap-2">
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="w-full ">
+                <FormControl className="w-full">
+                  <Input
+                    {...field}
+                    name="password"
+                    type={isVisible ? "text" : "password"}
+                    placeholder={"8+ Characters"}
+                    endContent={
+                      <Button
+                        className="bg-opacity-0 border-0 "
+                        variant="ghost"
+                        type="button"
+                        onClick={toggleVisibility}
+                      >
+                        {isVisible ? <EyeSlashFilledIcon /> : <EyeFilledIcon />}
+                      </Button>
+                    }
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="grid gap-2">
+          <div className="flex items-center">
+            {/* <Link href="#" className="ml-auto inline-block text-sm underline">
               Forgot your password?
             </Link> */}
-                    </div>
-                </div>
-                <FormError message={error} />
-                <FormSuccess message={success} />
+          </div>
+        </div>
+        <FormError message={error} />
+        <FormSuccess message={success} />
 
-                <Button type="submit" className="w-full">
-                    Login
-                </Button>
-            </form>
-        </Form>
-    )
-}
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+      </form>
+    </Form>
+  );
+};
 
-export default LoginForm
+export default LoginForm;
--- app/(auth)/signup/ui/signUpForm.tsx
+++ app/(auth)/signup/ui/signUpForm.tsx
@@ -1,134 +1,132 @@
-"use client"
-import * as z from "zod"
-import { useForm } from "react-hook-form"
-import { RegisterSchema } from "@/schemas"
-import FormError from "../../ui/form-error"
-import { signup } from "@/actions/actions"
-// import { Input } from "@/components/ui/input";
-import { Input } from "@/components/ui/gradient-input"
-import FormSuccess from "../../ui/form-success"
-import { Button } from "@/components/ui/button"
-import { startTransition, useState } from "react"
-import { zodResolver } from "@hookform/resolvers/zod"
+"use client";
 import {
-    Form,
-    FormField,
-    FormControl,
-    FormItem,
-    FormMessage,
-} from "@/components/ui/form"
+  Form,
+  FormField,
+  FormControl,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { Input } from "@nextui-org/input";
+import { RegisterSchema } from "@/schemas";
+import { signup } from "@/actions/actions";
+import FormError from "../../ui/form-error";
+import FormSuccess from "../../ui/form-success";
+import { Button } from "@/components/ui/button";
+import { startTransition, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { EyeFilledIcon } from "../../ui/eyeFilledIcon";
+import { EyeSlashFilledIcon } from "../../ui/eyeSlashedFilled";
 
 const SignUpForm = () => {
-    const [error, setError] = useState<string | undefined>("")
-    const [success, setSuccess] = useState<string | undefined>("")
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleVisibility = () => setIsVisible(!isVisible);
 
-    const form = useForm<z.infer<typeof RegisterSchema>>({
-        resolver: zodResolver(RegisterSchema),
-        defaultValues: {
-            name: "",
-            email: "",
-            password: "",
-        },
-    })
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
 
-    const onSubmit = async (values: z.infer<typeof RegisterSchema>) => {
-        // client-side-validation
-        setError("")
-        setSuccess("")
-        startTransition(() => {
-            signup(values).then((data) => {
-                if (data) {
-                    setError(data.error)
-                    setSuccess(data.success)
-                } else {
-                    return
-                }
-            })
-        })
-    }
+  const onSubmit = async (values: z.infer<typeof RegisterSchema>) => {
+    // client-side-validation
+    setError("");
+    setSuccess("");
+    startTransition(() => {
+      signup(values).then((data) => {
+        if (data) {
+          setError(data.error);
+          setSuccess(data.success);
+        } else {
+          return;
+        }
+      });
+    });
+  };
 
-    return (
-        <Form {...form}>
-            <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="grid gap-4 min-w-[320px] min-h-[200px]"
-            >
-                <div className="grid gap-2">
-                    <FormField
-                        control={form.control}
-                        name="name"
-                        render={({ field }) => (
-                            <FormItem className="w-full">
-                                <FormControl className="w-full">
-                                    <Input
-                                        {...field}
-                                        name="name"
-                                        placeholder="Full Name"
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                </div>
-                <div className="grid gap-2">
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem className="w-full">
-                                <FormControl className="w-full">
-                                    <Input
-                                        {...field}
-                                        name="email"
-                                        type="email"
-                                        placeholder="m@example.com"
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                </div>
-                <div className="grid gap-2">
-                    <FormField
-                        control={form.control}
-                        name="password"
-                        render={({ field }) => (
-                            <FormItem className="w-full ">
-                                <FormControl className="w-full">
-                                    <Input
-                                        {...field}
-                                        name="password"
-                                        type={"password"}
-                                        placeholder={"8+ Characters"}
-                                    />
-                                    {/* <div className="w-full flex items-end">
-                   
-                    <Button
-                      className="rounded-none border-l-0 border-r-0 border-t-0 border-b-2 "
-                      variant="outline"
-                      type="button"
-                      onClick={toggleVisibility}
-                    >
-                      {isVisible ? <EyeSlashFilledIcon /> : <EyeFilledIcon />}
-                    </Button>
-                  </div> */}
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                </div>
-                <FormError message={error} />
-                <FormSuccess message={success} />
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="grid gap-4 min-w-[320px] min-h-[200px]"
+      >
+        <div className="grid gap-2">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl className="w-full">
+                  <Input {...field} name="name" placeholder="Full Name" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="grid gap-2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl className="w-full">
+                  <Input
+                    {...field}
+                    name="email"
+                    type="email"
+                    placeholder="m@example.com"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="grid gap-2">
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="w-full ">
+                <FormControl className="w-full">
+                  <Input
+                    {...field}
+                    name="password"
+                    type={isVisible ? "text" : "password"}
+                    placeholder={"8+ Characters"}
+                    endContent={
+                      <Button
+                        className="bg-opacity-0 border-0 "
+                        variant="ghost"
+                        type="button"
+                        onClick={toggleVisibility}
+                      >
+                        {isVisible ? <EyeSlashFilledIcon /> : <EyeFilledIcon />}
+                      </Button>
+                    }
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormError message={error} />
+        <FormSuccess message={success} />
 
-                <Button type="submit" className="w-full">
-                    Create an account
-                </Button>
-            </form>
-        </Form>
-    )
-}
+        <Button type="submit" className="w-full">
+          Create an account
+        </Button>
+      </form>
+    </Form>
+  );
+};
 
-export default SignUpForm
+export default SignUpForm;
